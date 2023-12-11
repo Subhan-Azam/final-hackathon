3411f9fd75f2a75ffe496524dd24bdc9f5d73e9a
Subhan Azam <subhanshabeer2005@gmail.com>
2023-12-11 14:21:10 +0500

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  module.exports = nextConfig;
+    ignoreDuringBuilds: true,
+  },
+}
+module.exports = nextConfig
