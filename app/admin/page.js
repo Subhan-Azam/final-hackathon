6710706128d3186049fcce032edee0ef9d8eed3b
Subhan Azam <subhanshabeer2005@gmail.com>
2023-12-11 23:01:52 +0500

    initial commit

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,44 +1,49 @@
-// "use client"
-// import { db } from "@/config/firebase";
-// import { collection, getDoc, getDocs } from "firebase/firestore";
+"use client";
+import { db } from "@/config/firebase";
+import { collection, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-// export default function Admin() {
+export default function Admin() {
+  const [allPatients, setAllPatients] = useState([]);
 
-// const fetchDocs  = async ()=>{
+  const fetchDocs = async () => {
+    try {
+      const collectionName = collection(db, "patients");
+      const docs = await getDoc(collectionName);
+      const patientsData = [];
 
-//   try {
-//     const collectionName = collection(db, "patients")
-//     const docs = await getDoc(collectionName)
-// const patients = []
-//     docs.forEach(doc => {
-//       patients.push({
-//         id:doc.id,
-//         ...doc.data()
-//       })
-//     });
-    
-//     console.log("students", students)
+      docs.forEach((doc) => {
+        patientsData.push(doc.Data());
+      });
 
-//   } catch (error) {
-//     console.error("error", error)
-//   }
-// }
-//   return (
-//     <table>
-//       <tr><td>Name</td></tr>
-//       <tr><td>Contact</td></tr>
-//       <tr><td>Medical Condition</td></tr>
-//       {
-//       patients.map((patient)=>{
-//           return(
-//             <tr>
-//               <td>{student.name}</td>
-//               <td>{student.contact}</td>
-//               <td>{student.medical}</td>
-//             </tr>
-//           )
-//         })
-//       }
-//     </table>
-//   )
-// }
+      setAllPatients;
+      console.log("patientsData", patientsData);
+    } catch (error) {
+      console.error("error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDocs;
+  }, []);
+
+  return (
+    <table>
+      <tr>
+        <td>Name</td>
+        <td>Contact</td>
+        <td>Medical Condition</td>
+      </tr>
+
+      {allPatients.map((patient) => {
+        return (
+          <tr>
+            <td>{patient.name}</td>
+            <td>{patient.contact}</td>
+            <td>{patient.medical}</td>
+          </tr>
+        );
+      })}
+    </table>
+  );
+}
